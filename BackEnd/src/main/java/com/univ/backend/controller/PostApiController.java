2cdba79f86b837f8cbbb0f32396bd3389d489b46
Sangyong-Jeon <tkddyd420@naver.com>
2022-06-10 17:09:33 +0900

    [모바일,백] 과제 마무리
    - apk파일 생성
    - 스프링부트 jar 파일로 서버 제출
    위 두가지를 위한 약간의 코드 정리

--- BackEnd/src/main/java/com/univ/backend/controller/PostApiController.java
+++ BackEnd/src/main/java/com/univ/backend/controller/PostApiController.java
@@ -9,12 +9,10 @@ import com.univ.backend.util.ResponseUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
